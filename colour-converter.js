36bd376e5ccbd6446cff29e86f2a6630f9bc0a2b
Stephen <stephen_h369@live.co.uk>
2020-05-19 15:22:48 +0100

    edit

--- colour-converter.js
+++ colour-converter.js
@@ -1,12 +1,19 @@
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+const selector1 = document.querySelector("#selector1")
+const selector2 = document.querySelector("#selector2")
+console.log(selector1.children)
+console.log(selector2.children)
 
-btn.addEventListener("click", function(){
-    const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
-        const hex = x.toString(16)
-        return hex.length === 1 ? '0' + hex : hex
-    }).join('')
-    
+if (selector1.children == [0] && selector2.children == [1]) {
+    btn.addEventListener("click", function(){
+        const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
+            const hex = x.toString(16)
+     return hex.length === 1 ? '0' + hex : hex
+        }).join('')
+        
+        
+    })
     const rgbToHex = document.body.style.background = rgbToHex
-    color.textContent = rgbToHex
-})
+        color.textContent = rgbToHex
+}
